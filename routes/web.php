136fc2db6799de2edef6ed78ac8fedaa99556403
AdityaDarmaKesuma <adityadarmakesuma@gmail.com>
2023-05-20 21:01:21 +0700

    Tugas Praktikum10

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\FormController;
+use App\Http\Controllers\SkillController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -46,3 +47,9 @@ Route::get('/form', [FormController::class, 'index']);
 
 //buat route hasil form
 Route::POST('/hasil', [FormController::class, 'hasil']);
+
+//buat route skill
+Route::get('/skill',[SkillController::class, 'index']);
+
+//buat route hasilskill
+Route::POST('/hasilskill',[SkillController::class, 'skill']);
